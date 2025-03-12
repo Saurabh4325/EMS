b1ce96a7400bc791201c68dc4e101f631ad66a7b
Saurabh Kumar <sbhk4325@gmail.com>
2025-03-12 13:42:59 +0530

    fix some problem

--- src/App.jsx
+++ src/App.jsx
@@ -6,28 +6,27 @@ import { getLocalStorage, setLocalStorage } from './utils/localStorage'
 
 
 const App = () => {
-  const [user, setUser] = useState('null')
+  const [user, setUser] = useState(null)
 
-  const handleLogin = (email,password)=>{
-    console.log(email,password)
-    if(email==='admin@example.com' && password==='123'){
-      console.log("this is admin")
-      setUser('admin')
-      console.log(user)
-    }else if(email==='employee1@example.com' && password==='123'){
-      console.log("this is employe pannel")
-      setUser('employee')
-      console.log(user)
-    }else{
-      alert('Invalid Credentials')
+  const handleLogin = (email, password) => {
+    if (email === "admin@example.com" && password === "123") {
+      setUser("admin");
+    } else if (email === "employee1@example.com" && password === "123") {
+      setUser("employee");
+    } else {
+      alert("Invalid Credentials");
     }
-  }
+  };
   // handleLogin('employee1@example.com','123')
   return (
     <>
-      {!user ? <Login handleLogin={handleLogin} /> : '' }
-      {/* <EmployDashBorad /> */}
-      {/* <AdminDashBorad /> */}
+     {!user ? (
+        <Login handleLogin={handleLogin} />
+      ) : user === "admin" ? (
+        <AdminDashBorad />
+      ) : (
+        <EmployDashBorad />
+      )}
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,15 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import "tailwindcss";
-import AuthContext from './context/AuthContext.jsx';
-import TaskContext from './context/TaskContext.jsx';
+import AuthProvider from './context/AuthProvider.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <TaskContext>
-    <AuthContext>
+    <AuthProvider>
     <App />
-    </AuthContext>
-    </TaskContext>
+    </AuthProvider>
   </StrictMode>,
 )
